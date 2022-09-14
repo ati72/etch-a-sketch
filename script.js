3fcf616e92ff6216e1decb36817870a82a7ee0c3
ati72 <hegedusattila@protonmail.com>
2022-09-14 17:04:53 +0200

    working on changing grid size

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
 const sketchContainer = document.querySelector('.sketch-container');
-let rows = 1;
+const btnSmallPixel = document.querySelector('#pixel-small');
+const btnMidPixel = document.querySelector('#pixel-mid');
+const btnBigPixel = document.querySelector('#pixel-big');
+let rows = 8;
 let squareCount = rows**2;
 let cssText = "";
 
+btnMidPixel.addEventListener('click', () => {
+    rows = 16;
+    cssText = "";
+    sketchContainer.style.gridTemplateColumns = cssText;
+
+    for(i=0; i<rows; i++) {
+        cssText += "1fr ";
+        sketchContainer.style.gridTemplateColumns = cssText;
+    }
+    
+    for(i=0; i<squareCount; i++) {
+        let  newSquare = document.createElement('div');
+        newSquare.classList.add('square');
+        sketchContainer.appendChild(newSquare);
+    }
+});
+
+/* function pixelSize(button) {
+    if (button.value === "Big") rows = 8;
+    else if (button.value === "Medium") rows = 16;
+    else if (button.value === "Small") rows = 32;
+}
+ */
+
+/* rowRange.addEventListener("input", () => {
+    rows = rowRange.valueAsNumber;
+    sketchContainer.style.gridTemplateColumns = "";
+    cssText = "";
+    console.log(rows);
+
+
+    for(i=0; i<rows; i++) {
+       
+        cssText += "1fr ";
+        console.log(cssText);
+        sketchContainer.style.gridTemplateColumns = cssText;
+    }
+    
+    for(i=0; i<squareCount; i++) {
+        let  newSquare = document.createElement('div');
+        newSquare.classList.add('square');
+        sketchContainer.appendChild(newSquare);
+    }
+
+}) */
 
 for(i=0; i<rows; i++) {
     cssText += "1fr ";
@@ -13,5 +61,5 @@ for(i=0; i<squareCount; i++) {
     let  newSquare = document.createElement('div');
     newSquare.classList.add('square');
     sketchContainer.appendChild(newSquare);
-   
 }
+
